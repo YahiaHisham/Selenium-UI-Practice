a54c427500eb649c98d4ca5311382ff23c136ca3
YahiaElaydyy <y.elaydy@foodics.com>
2023-04-10 22:20:23 +0200

    user registration full scenario

--- src/main/java/pages/PageBase.java
+++ src/main/java/pages/PageBase.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,24 +17,26 @@ public class PageBase {
         this.driver = driver;
     }
 
-    public PageBase clickOnElement(By elementLocator) {
+    public void clickOnElement(By elementLocator) {
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.presenceOfElementLocated(elementLocator));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", elementLocator);
+        scrollToElementView(elementLocator);
         driver.findElement(elementLocator).click();
-        return this;
     }
 
-    public PageBase setElementText(By elementLocator, String text) {
+    public void scrollToElementView(By elementLocator) {
+        WebElement element = driver.findElement(elementLocator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void setElementText(By elementLocator, String text) {
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.presenceOfElementLocated(elementLocator));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", elementLocator);
+        scrollToElementView(elementLocator);
         driver.findElement(elementLocator).sendKeys(text);
-        return this;
     }
 
-    public PageBase clickOnSignupLoginButton() {
+    public void clickOnSignupLoginButton() {
         clickOnElement(signupLoginButton);
-        return this;
     }
 }
