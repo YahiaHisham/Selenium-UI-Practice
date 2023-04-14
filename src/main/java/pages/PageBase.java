d7bcb343eb1c9debafc17f19de39d8c96c4bc4e1
YahiaElaydyy <y.elaydy@foodics.com>
2023-04-14 22:10:58 +0200

    adding generate random number and random text to page Base

--- src/main/java/pages/PageBase.java
+++ src/main/java/pages/PageBase.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class PageBase {
     private final By signupLoginButton = By.xpath("//a[text()=' Signup / Login']");
@@ -38,5 +39,26 @@ public class PageBase {
 
     public void clickOnSignupLoginButton() {
         clickOnElement(signupLoginButton);
+    }
+
+    public static String generateRandomText(int length) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            char c = alphabet.charAt(random.nextInt(alphabet.length()));
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+    public static String generateRandomNumbers(int length) {
+        String digits = "0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            char c = digits.charAt(random.nextInt(digits.length()));
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
