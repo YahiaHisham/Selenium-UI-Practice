2c0027e3d0d2e7873a057d3f86e81113bc05186c
YahiaElaydyy <y.elaydy@foodics.com>
2023-07-03 18:37:35 +0300

    adding test cases page class and test class

---
+++ src/main/java/pages/TestCasesPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestCasesPage extends PageBase{
+    public TestCasesPage(WebDriver driver) {
+        super(driver);
+    }
+}
