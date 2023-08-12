20c89fd0c376879d3f8218ef0b1ac3ce4ec32aff
YahiaElaydyy <y.elaydy@foodics.com>
2023-08-12 16:44:06 +0300

    finished Test Case 11: Verify Subscription in Cart page
    In conclusion :
    finished : Test Case 9: Search Product Test Case 10: Verify Subscription in home page
    Test Case 11: Verify Subscription in Cart page

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,10 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class CartPage extends PageBase{
+    private static final By emailSubscriptionField = By.id("susbscribe_email");
+    private static final By subscribeButton = By.id("subscribe");
+    private static final By successSubscriptionAlert = By.cssSelector("alert-success");
     public CartPage(WebDriver driver) {
         super(driver);
     }
+    public CartPage enterSubscriptionEmail() {
+        setElementText(emailSubscriptionField, generateRandomText(6) + "@mail.com");
+        return this;
+    }
 
+    public CartPage clickOnSubscribeButton() {
+        clickOnElement(subscribeButton);
+        return this;
+    }
+    public void assertThatUserIsSubscribedSuccessfully() {
+        Assert.assertEquals(driver.findElement(successSubscriptionAlert).getText(), "You have been successfully subscribed");
+    }
 }
--- src/test/java/SubscriptionTest.java
+++ src/test/java/SubscriptionTest.java
@@ -1,4 +1,5 @@
 import org.testng.annotations.Test;
+import pages.CartPage;
 import pages.HomePage;
 
 public class SubscriptionTest extends TestBase {
@@ -11,6 +12,11 @@ public class SubscriptionTest extends TestBase {
     }
     @Test
     public void assertThatUserCanSubscribeForUpdatesFromCartPage() {
-
+        new HomePage(driver)
+                .clickOnCartButton();
+        new CartPage(driver)
+                .enterSubscriptionEmail()
+                .clickOnSubscribeButton()
+                .assertThatUserIsSubscribedSuccessfully();
     }
 }
